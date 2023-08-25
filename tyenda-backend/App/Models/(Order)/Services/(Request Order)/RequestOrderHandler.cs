a9f13429abeace9e99dc9c3c7a1c9cc536a7e918
Peter Abou Absi <peteraba60@gmail.com>
2023-08-25 15:09:22 +0300

    Update

--- tyenda-backend/App/Models/(Order)/Services/(Request Order)/RequestOrderHandler.cs
+++ tyenda-backend/App/Models/(Order)/Services/(Request Order)/RequestOrderHandler.cs
@@ -4,8 +4,11 @@ using System.Threading.Tasks;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using tyenda_backend.App.Context;
+using tyenda_backend.App.Models._Alert_;
+using tyenda_backend.App.Models._Notification_;
 using tyenda_backend.App.Models._OrderItem_;
 using tyenda_backend.App.Models.Enums;
+using tyenda_backend.App.Services.Notification_Service;
 using tyenda_backend.App.Services.Token_Service;
 
 namespace tyenda_backend.App.Models._Order_.Services._Request_Order_
@@ -14,11 +17,13 @@ namespace tyenda_backend.App.Models._Order_.Services._Request_Order_
     {
         private readonly TyendaContext _context;
         private readonly ITokenService _tokenService;
+        private readonly INotificationService _notificationService;
 
-        public RequestOrderHandler(TyendaContext context, ITokenService tokenService)
+        public RequestOrderHandler(TyendaContext context, ITokenService tokenService, INotificationService notificationService)
         {
             _context = context;
             _tokenService = tokenService;
+            _notificationService = notificationService;
         }
 
         public async Task<Guid> Handle(RequestOrder request, CancellationToken cancellationToken)
@@ -27,11 +32,16 @@ namespace tyenda_backend.App.Models._Order_.Services._Request_Order_
             {
                 var accountId = _tokenService.GetHeaderTokenClaim(Constants.AccountId);
                 var customer = await _context.Customers.SingleOrDefaultAsync(customer => customer.AccountId == Guid.Parse(accountId), cancellationToken);
-                if (customer == null)
-                {
-                    throw new UnauthorizedAccessException("Customer not found");
-                }
+                
+                if (customer == null) throw new UnauthorizedAccessException("Customer not found");
+                
+                var item = await _context.Items
+                    .Include(item => item.Store)
+                    .ThenInclude(item => item!.Account)
+                    .SingleOrDefaultAsync(item => item.Id == Guid.Parse(request.RequestOrderForm.ItemId), cancellationToken);
 
+                if (item == null) throw new Exception("Item not found");
+                
                 var newOrder = new Order()
                 {
                     Id = Guid.NewGuid(),
@@ -115,6 +125,34 @@ namespace tyenda_backend.App.Models._Order_.Services._Request_Order_
                     }
                 }
 
+                var newNotification = new Notification()
+                {
+                    Id = Guid.NewGuid(),
+                    Title = "<p><b>New Order Requested</b></p>",
+                    Description = "<p>You have just received a new order from " + customer.Firstname + " " +
+                                  customer.Lastname + ". Click to see order’s details.</p>",
+                    CreatedAt = DateTime.UtcNow,
+                    CustomerId = customer.Id,
+                    Link = "/application/store/order/"+newOrder.Id
+                };
+                
+                var newAlert = new Alert()
+                {
+                    NotificationId = newNotification.Id,
+                    AccountId = item.Store!.AccountId
+                };
+
+                await _context.Notifications.AddAsync(newNotification, cancellationToken);
+                await _context.Alerts.AddAsync(newAlert, cancellationToken);
+                
+                //Send Notification to store
+                if (item.Store!.Account!.OnOrder)
+                {
+                    string title = "New Order Requested";
+                    string message = "You have just received a new order from " + customer.Firstname + " " + customer.Lastname + ". Click to see order’s details.";
+                    string route = "/application/store/order/"+newOrder.Id;
+                    _notificationService.SendNotificationAsync(item.Store.AccountId.ToString(), title, message, route);
+                }
                 await _context.SaveChangesAsync(cancellationToken);
 
                 return newOrder.Id;
